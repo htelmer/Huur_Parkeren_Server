23c7de6c01e5fd8b459d9f38d898f7981fe3f1d0
htelmer <htelmer86@gmail.com>
2022-05-31 16:12:24 +0200

    seed areaOwners

--- seeders/20220531120311-areaOwners.js
+++ seeders/20220531120311-areaOwners.js
@@ -6,7 +6,7 @@ module.exports = {
       "areaOwners",
       [
         {
-          userId: 1,
+          ownerId: 1,
           areaId: 1,
           createdAt: new Date(),
           updatedAt: new Date(),
